e731206beca55e0beea6ffde6183d69e59adf1be
Apprentice <apprentice@dbc17.local>
2013-10-14 18:31:53 -0700

    play with event handlers and canvas to draw on canvas

--- canvas.js
+++ canvas.js
@@ -1,7 +1,26 @@
 window.onload=function(){
-var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
-
-//JS code goes in here
-
+  var my_canvas = document.getElementById("myCanvas");
+  my_canvas.addEventListener("mousemove", getPosition, false);
 };
+
+
+
+function getPosition(event){
+  var canvas = document.getElementById("myCanvas");
+  
+  var x = new Number();
+  var y = new Number();
+
+  x = event.x;
+  y = event.y;
+
+  x -= canvas.offsetLeft;
+  y -= canvas.offsetTop;
+
+  console.log("x is: " + x + "  y is : " + y);
+
+  my_canvas = canvas.getContext('2d');
+  my_canvas.fillStyle = 'red';
+  my_canvas.fillRect(x,y,5,5);
+}
+
