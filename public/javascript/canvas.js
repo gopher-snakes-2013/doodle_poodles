337f1806aa3f1710087589d6d2d0a52a19d7a1fc
Steven Nugent <stevennugentdev@gmail.com>
2013-10-16 16:56:41 -0700

    Testing touch screen feature

--- public/javascript/canvas.js
+++ public/javascript/canvas.js
@@ -2,20 +2,20 @@ window.onload=function(){
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
-  canvas.addEventListener('mousedown', startTracking, false);
+  canvas.addEventListener('touchstart', startTracking, false);
   
   function startTracking(){
     x = event.x - canvas.offsetLeft;
     y = event.y - canvas.offsetTop;
-    canvas.addEventListener('mousemove', getPosition, false);
-    if (canvas.addEventListener('mouseup', stopTracking, false)){
+    canvas.addEventListener('touchmove', getPosition, false);
+    if (canvas.addEventListener('touchend', stopTracking, false)){
 
     }
     
   }
 
   function stopTracking(){
-    canvas.removeEventListener('mousemove', getPosition, false);
+    canvas.removeEventListener('touchmove', getPosition, false);
 
   }
 
