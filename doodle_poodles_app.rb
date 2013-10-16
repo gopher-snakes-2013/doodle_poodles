8dcb35aaaffe52182f0e1693fbf7115eb4a9360c
bbeaird <bbeaird@gmail.com>
2013-10-15 20:06:55 -0700

    Spike: push javascript and ccs containing index.erb to heroku Used bare bones heroku config files where possible

--- doodle_poodles_app.rb
+++ doodle_poodles_app.rb
@@ -3,14 +3,12 @@ require 'sinatra'
 
 set :database, ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || "sqlite3:///db/poodles.db")
 
+# Note for our own learning, feedback appreciated :-)
 # This works: 
 # set :database, ActiveRecord::Base.establish_connection("sqlite3:///db/poodles.db")
 # This does not work:
 # set :database, ActiveRecord::Base.establish_connection('postgres://localhost/poodles')
 
-# DATABASE_URL='postgres://localhost/thunder_dev'
-
 get '/' do
-
   erb :index
 end
