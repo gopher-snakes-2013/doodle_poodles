b4d373a74108816c2958a9bdeccb3611f3295613
Steven Nugent <stevennugentdev@gmail.com>
2013-10-17 15:36:42 -0700

    testing accessing old touches

--- public/javascript/canvas.js
+++ public/javascript/canvas.js
@@ -9,6 +9,9 @@ function getPosition() {
   x = event.targetTouches[0].pageX;
   y = event.targetTouches[0].pageY;
 
+  var touches = event.targetTouches;
+  alert(touches[0] + ", " + touches[1] + ", " + touches[2]);
+
   draw_it.fillStyle = "red";
   draw_it.fillRect(x, y, 10, 10);
   // draw_it.strokeStyle = 'red';
