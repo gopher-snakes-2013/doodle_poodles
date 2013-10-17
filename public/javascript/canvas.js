d91944535967f30e12eef3cb7f80b6e03055d5bc
bbeaird <bbeaird@gmail.com>
2013-10-16 18:13:09 -0700

    erase and clear functions

--- public/javascript/canvas.js
+++ public/javascript/canvas.js
@@ -9,14 +9,11 @@ var ctx = canvas.getContext('2d');
     y = event.y - canvas.offsetTop;
     canvas.addEventListener('mousemove', getPosition, false);
     if (canvas.addEventListener('mouseup', stopTracking, false)){
-
     }
-    
   }
 
   function stopTracking(){
     canvas.removeEventListener('mousemove', getPosition, false);
-
   }
 
   function getPosition(){
@@ -27,11 +24,33 @@ var ctx = canvas.getContext('2d');
     x = event.x - canvas.offsetLeft - 5;
     y = event.y - canvas.offsetTop - 5;
 
-  draw_it = canvas.getContext('2d');
-  draw_it.strokeStyle = 'red';
-  draw_it.beginPath();
-  draw_it.moveTo(xOld,yOld);
-  draw_it.lineTo(x,y);
-  draw_it.stroke();
-  } 
+    draw_it = canvas.getContext('2d');
+    draw_it.strokeStyle = 'red';
+    draw_it.beginPath();
+    draw_it.moveTo(xOld,yOld);
+    draw_it.lineTo(x,y);
+    draw_it.stroke();
+    }
+
+var clear = document.getElementById("clear");
+clear.addEventListener('click', clearCanvas, false);
+
+ function clearCanvas(){
+    ctx.clearRect(0, 0, 640, 360);
+  }
+
+var erase = document.getElementById("erase");
+erase.addEventListener('click', engageErase, false);
+
+  function engageErase(){
+    var eraseCanvas = document.getElementById("myCanvas");
+    eraseCanvas.addEventListener('click', eraseTheCanvas, false);
+    function eraseTheCanvas(){
+      x = event.x - canvas.offsetLeft - 5;
+      y = event.y - canvas.offsetTop - 5;
+      ctx.clearRect(x, y, 10, 10);
+    }
+  }
+
+ 
 };
