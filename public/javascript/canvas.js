d1973d7096a6d37436c64da2a12a863dd9792ce2
bbeaird <bbeaird@gmail.com>
2013-10-17 16:39:15 -0700

    working touch smooth line

--- public/javascript/canvas.js
+++ public/javascript/canvas.js
@@ -1,19 +1,27 @@
 function initialize() {
   var canvas = document.getElementById("myCanvas");
   draw_it = canvas.getContext('2d');
+  canvas.addEventListener('touchstart', initializePosition, false);
   canvas.addEventListener('touchmove', getPosition, false);
 };
 
-function getPosition() {
+function initializePosition() {
   event.preventDefault();
+  x = event.targetTouches[0].pageX;
+  y = event.targetTouches[0].pageY; 
+}
+
+function getPosition() {
+  var xOld = x;
+  var yOld = y;
   x = event.targetTouches[0].pageX;
   y = event.targetTouches[0].pageY;
 
-  draw_it.fillStyle = "red";
-  draw_it.fillRect(x, y, 10, 10);
-  // draw_it.strokeStyle = 'red';
-  // draw_it.beginPath();
-  // draw_it.moveTo(oldX,oldY);
-  // draw_it.lineTo(x,y);
-  // draw_it.stroke();
+
+  var touches = event.targetTouches;
+  draw_it.strokeStyle = 'red';
+  draw_it.beginPath();
+  draw_it.moveTo(x,y);
+  draw_it.lineTo(xOld,yOld);
+  draw_it.stroke();
 };
