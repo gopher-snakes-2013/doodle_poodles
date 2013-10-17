9e18e874b62ffa6e47c5ec0359002cdb5500cef0
Steven Nugent <stevennugentdev@gmail.com>
2013-10-16 18:50:54 -0700

    testing touchmove alert

--- public/javascript/canvas.js
+++ public/javascript/canvas.js
@@ -1,34 +1,35 @@
 function initialize() {
   var canvas = document.getElementById("myCanvas");
-  canvas.addEventListener('touchstart', startTracking, false);
+  // canvas.addEventListener('touchstart', startTracking, false);
   canvas.addEventListener('touchmove', getPosition, false);
 };
   
-function startTracking(){
-  event.preventDefault();
+// function startTracking(){
+//   event.preventDefault();
 
 
-  x = event.targetTouches[0].pageX
-  y = event.targetTouches[0].pageY;
+//   x = event.targetTouches[0].pageX
+//   y = event.targetTouches[0].pageY;
 
-  // canvas.addEventListener('touchmove' getPosition, false);
+//   // canvas.addEventListener('touchmove' getPosition, false);
 
-  // canvas.addEventListener('touchmove', getPosition, false);
-  // if (canvas.addEventListener('touchend', stopTracking, false)){}
-};
+//   // canvas.addEventListener('touchmove', getPosition, false);
+//   // if (canvas.addEventListener('touchend', stopTracking, false)){}
+// };
 
 function getPosition() {
-  oldX = x;
-  oldY = y;
+  // oldX = x;
+  // oldY = y;
   x = event.targetTouches[0].pageX
   y = event.targetTouches[0].pageY;
+  alert(x)
 
-  draw_it = canvas.getContext('2d');
-  draw_it.strokeStyle = 'red';
-  draw_it.beginPath();
-  draw_it.moveTo(oldX,oldY);
-  draw_it.lineTo(x,y);
-  draw_it.stroke();
+  // draw_it = canvas.getContext('2d');
+  // draw_it.strokeStyle = 'red';
+  // draw_it.beginPath();
+  // draw_it.moveTo(oldX,oldY);
+  // draw_it.lineTo(x,y);
+  // draw_it.stroke();
 
 };
 
