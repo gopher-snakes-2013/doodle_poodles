4844804a6840835b6783756d163c11ef6b837c30
Steven Nugent <stevennugentdev@gmail.com>
2013-10-16 19:26:35 -0700

    test targetTouches

--- public/javascript/canvas.js
+++ public/javascript/canvas.js
@@ -8,11 +8,11 @@ function getPosition() {
   event.preventDefault();
   x = event.targetTouches[0].pageX;
   y = event.targetTouches[0].pageY;
-  // oldX = targetTouches[1]
-  // oldY = targetTouches[1]
+  oldX = targetTouches[1]
+  oldY = targetTouches[1]
 
   draw_it.fillStyle = "red";
-  draw_it.fillRect(x, y, 10, 10);
+  draw_it.fillRect(oldX, oldY, 10, 10);
   // draw_it.strokeStyle = 'red';
   // draw_it.beginPath();
   // draw_it.moveTo(oldX,oldY);
