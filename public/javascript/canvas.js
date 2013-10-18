c56f4bb9693bae1dc44d1b71649f76da96710b20
Zee Spencer <zee@zeespencer.com>
2013-10-18 15:00:08 -0400

    Stop putting x,y xOld, yOld into the global scope

--- public/javascript/canvas.js
+++ public/javascript/canvas.js
@@ -1,24 +1,20 @@
 window.onload=function(){
-var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
+  var canvas = document.getElementById('myCanvas');
 
   canvas.addEventListener('mousedown', startTracking, false);
-  
+
   function startTracking(){
     x = event.x - canvas.offsetLeft;
     y = event.y - canvas.offsetTop;
     canvas.addEventListener('mousemove', getPosition, false);
-    if (canvas.addEventListener('mouseup', stopTracking, false)){
-
-    }
-    
+    canvas.addEventListener('mouseup', stopTracking, false)
   }
 
   function stopTracking(){
     canvas.removeEventListener('mousemove', getPosition, false);
-
   }
 
+  var xOld, yOld;
   function getPosition(){
 
     xOld = x;
@@ -27,11 +23,11 @@ var ctx = canvas.getContext('2d');
     x = event.x - canvas.offsetLeft - 5;
     y = event.y - canvas.offsetTop - 5;
 
-  draw_it = canvas.getContext('2d');
-  draw_it.strokeStyle = 'red';
-  draw_it.beginPath();
-  draw_it.moveTo(xOld,yOld);
-  draw_it.lineTo(x,y);
-  draw_it.stroke();
-  } 
+    draw_it = canvas.getContext('2d');
+    draw_it.strokeStyle = 'red';
+    draw_it.beginPath();
+    draw_it.moveTo(xOld,yOld);
+    draw_it.lineTo(x,y);
+    draw_it.stroke();
+  }
 };
